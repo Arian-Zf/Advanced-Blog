240ee110841848e632472b7d294fa1753e94ce9d
Arian <arianzafari2019@gmail.com>
2026-06-30 23:43:28 +0330

    install pillow & fix & setting(user manager config)

--- core/core/settings.py
+++ core/core/settings.py
@@ -42,7 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'blog.apps.BlogConfig'
+    'blog.apps.BlogConfig',
     'accounts.apps.AccountsConfig'
 ]
 
@@ -127,3 +127,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+#user manager config
+AUTH_USER_MODEL = 'accounts.User'
