53b0530503cdcec5c45f48cc0dc5fa6c5c182a56
Arian <arianzafari2019@gmail.com>
2026-07-04 18:23:29 +0330

    TemplateView

--- core/blog/urls.py
+++ core/blog/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from . import views
+from django.views.generic import TemplateView
 
 
 
 urlpatterns = [
-    path('',views.indexView,name='indexView'),
+    path('num1',views.indexView,name='indexView'),
+    path('num2', TemplateView.as_view(template_name="index.html", extra_context={"name": "ali"})),
 ]
