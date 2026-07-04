d1fd3fc705684c2fb37a66eb62e33e42b15dfdb4
Arian <arianzafari2019@gmail.com>
2026-07-04 20:00:41 +0330

    TemplateView (views)

--- core/blog/urls.py
+++ core/blog/urls.py
@@ -6,5 +6,6 @@ from django.views.generic import TemplateView
 
 urlpatterns = [
     path('num1',views.indexView,name='indexView'),
-    path('num2', TemplateView.as_view(template_name="index.html", extra_context={"name": "ali"})),
+    # path('num2', TemplateView.as_view(template_name="index.html", extra_context={"name": "ali"})),
+    path('cbv-index', views.IndexView.as_view(),name='cbv-index'),
 ]
--- core/blog/views.py
+++ core/blog/views.py
@@ -1,7 +1,19 @@
 from django.shortcuts import render
+from .models import *
+from django.views.generic.base import TemplateView
 
 
 
 
 def indexView(request):
     return render(request,'index.html')
+
+
+class IndexView(TemplateView):
+    template_name = 'index.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["name"] = "ali"
+        context["posts"] = Post.objects.all()
+        return context
