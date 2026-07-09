febcd1d7ae9dacba27b0566a4aa18b06d5052638
Arian <arianzafari2019@gmail.com>
2026-07-09 21:37:10 +0330

    serializers.Serializer

--- core/blog/api/v1/views.py
+++ core/blog/api/v1/views.py
@@ -1,5 +1,7 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from ..serializers import PostSerializer
+from blog.models import Post
 
 
 @api_view()
@@ -10,7 +12,9 @@ def postlist(request):
 
 @api_view()
 def post_detail(request,id):
-    return Response(id)
+    post = Post.objects.get(id=id)
+    serializers = PostSerializer(post) # --> Dic
+    return Response(serializers.data) # --> json
 
 
-     
+    
