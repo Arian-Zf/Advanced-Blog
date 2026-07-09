b75f81d5ecab28df8778523c491b2d147454f9f1
Arian <arianzafari2019@gmail.com>
2026-07-09 22:11:58 +0330

    add many field to Posts & show list

--- core/blog/api/v1/views.py
+++ core/blog/api/v1/views.py
@@ -8,13 +8,15 @@ from django.shortcuts import get_object_or_404
 
 @api_view()
 def postlist(request):
-    return Response("ok")
+    posts = Post.objects.filter(status=True)
+    serializers = PostSerializer(posts,many=True)
+    return Response(serializers)
 
 
 
 @api_view()
 def post_detail(request,id):
-    post = get_object_or_404(Post,pk=id)
+    post = get_object_or_404(Post,pk=id,status=True)
     serializers = PostSerializer(post) # --> Dic
     return Response(serializers.data) # --> json
     # except Post.DoesNotExist:
