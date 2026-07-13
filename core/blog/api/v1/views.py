a92d39138cdb033cd3af7a33027b12dfe02b0c29
Arian <arianzafari2019@gmail.com>
2026-07-13 18:03:56 +0330

    Add permission ->IsAuthenticated,IsAuthenticatedOrReadOnly,IsAdminUser

--- core/blog/api/v1/views.py
+++ core/blog/api/v1/views.py
@@ -1,9 +1,11 @@
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
 from ..serializers import PostSerializer
 from blog.models import Post
 from rest_framework import status
 from django.shortcuts import get_object_or_404
+from rest_framework.permissions import IsAuthenticated,IsAuthenticatedOrReadOnly,IsAdminUser
+
 
 
 # @api_view()
@@ -13,6 +15,7 @@ from django.shortcuts import get_object_or_404
 #     return Response(serializers)
 
 @api_view(["GET", "POST"])
+@permission_classes([IsAuthenticated])
 def postList(request):
     if request.method == "GET":
         posts = Post.objects.filter(status=True)
