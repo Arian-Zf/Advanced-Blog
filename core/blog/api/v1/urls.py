3cfdd0acc4dd0a34f8c86c2bff86f0eeb4584017
Arian <arianzafari2019@gmail.com>
2026-07-15 17:02:41 +0330

    Add DefaultRouter to Urls

--- core/blog/api/v1/urls.py
+++ core/blog/api/v1/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path,include
 from . import views
+from rest_framework.routers import DefaultRouter
 
+router = DefaultRouter()
+router.register('post',views.PostViewSet,basename='post')
 
-
+app_name = 'api-v1'
 
 urlpatterns = [
 
@@ -13,7 +16,7 @@ urlpatterns = [
     # path('post/', views.PostList.as_view(),name='post_detail'),
     # path('postDetail/<int:pk>/', views.PostDetail.as_view(),name='post_detail'),
 
-    path('post/', views.PostViewSet.as_view({'get':'list','post':'create'}), name='post-list'),
-    path('post/<int:pk>/', views.PostViewSet.as_view({'get':'retrieve','put':'update','patch':'partial_update','delete':'destroy'}), name='post-detail'),
-
+    # path('post/', views.PostViewSet.as_view({'get':'list','post':'create'}), name='post-list'),
+    # path('post/<int:pk>/', views.PostViewSet.as_view({'get':'retrieve','put':'update','patch':'partial_update','delete':'destroy'}), name='post-detail'),
+    path('',include(router.urls))
 ]
