793f9992f2ce4d348fa37ae8b24ec823ee2217bc
Arian <arianzafari2019@gmail.com>
2026-07-18 22:04:55 +0330

    add CategorySerializer to PostSerializer

--- core/blog/api/serializers.py
+++ core/blog/api/serializers.py
@@ -6,26 +6,35 @@ from blog.models import Post,Category
 #     id = serializers.IntegerField()
 #     title = serializers.CharField(max_length=255)
 
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = ['id','name']
+
+
 class PostSerializer(serializers.ModelSerializer):
 
-    snippet = serializers.ReadOnlyField(source = 'get_snippet')
-    content = serializers.ReadOnlyField()
+    # snippet = serializers.ReadOnlyField(source = 'get_snippet')
+    # content = serializers.ReadOnlyField()
     relative_url = serializers.URLField(source='get_absolute_api_url', read_only=True)
     absolute_url = serializers.SerializerMethodField()
+    # category= serializers.SlugRelatedField(slug_field='name', queryset=Category.objects.all(),many=False)
+    category = CategorySerializer(read_only=False)
 
     class Meta:
         model = Post
-        fields = ['id','author','absolute_url','title','content','snippet','status','created_date','published_date','relative_url']
+        fields = ['id','author','absolute_url','category','title','content','status','created_date','published_date','relative_url']
         # read_only_fields = ['content']
 
     def get_absolute_url(self, obj):
         request = self.context.get('request')
         return request.build_absolute_uri(obj.pk)
+
+
+    # def to_representation(self, instance):
+    #     return super().to_representation(instance)
     
     
     
-class CategorySerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Category
-        fields = ['id','name']
 
